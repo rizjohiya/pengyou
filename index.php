e9ebc4f2bde9c626259a9a84f2189c7b1ae66007
Rizwan Joiya <rizjohiya@gmail.com>
2017-07-14 15:29:28 -0700

    add files test and some in index.php

--- index.php
+++ index.php
@@ -1,13 +1,39 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>homepage</title>
-	<link rel="stylesheet" href="">
+
+<meta charset="utf-8">
+
+<title>Experiment</title>
+	
+<style media="all">
+.wrap {background: #343436; padding: 12px; float: left; display: table;}
+.wrap-inner {display: table-row; background: #232323; }
+.left, .right {float: left; display: table-cell;}
+.left {width: 123px; border: 5px solid #232323; background: blue;}
+.left img {width: 123px; height: 208px; display: block;}
+.right {background: #2e2e30; width: 155px; min-height: 218px;
+border-width: 9px;
+border-style: solid;
+border-color: #343436 #343436 #343436 #010101;
+}
+</style>
+	
 </head>
+
 <body>
-	<h1>this is simple test program</h1>
-	<p>its first git project</p>
-</body>
-</html>
+
+<div class="wrap">
+  <div class="wrap-inner">
+	<div class="left">
+		<img src="" alt="">
+	</div>
+	<div class="right">
+            <p>Text text text text text text text text text</p>
+	    <p>Text text text text text text text text text</p>
+	    <p>Text text text text text text text text text</p>
+	    <p>Text text text text text text text text text</p>
+	</div>
+  </div>
+</div>
